724e999d2f49c6a4c0842ab665a0f72aff842307
c0b2313978 <c0b2313978@edu.teu.ac.jp>
2024-11-19 15:30:30 +0900

    練習1: 背景画像の読み込みとSurfaceの生成

--- pygame_image.py
+++ pygame_image.py
@@ -11,13 +11,22 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
+
+    # kokaton = pg.image.load("fig/3.png")
+    # kokaton = pg.transform.flip(kokaton, True, False)
+    
+    # x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        # screen.blit(kokaton, [x, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1     
+        # x += 1
+        # if x > 800:
+        #     x = 0
         clock.tick(10)
 
 
